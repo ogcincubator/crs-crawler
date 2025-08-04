5fdeb229b58ae3fce052a5814b635e411ff84f26
Alejandro Villar <avillar@ogc.org>
2025-08-04 15:59:26 +0200

    Lookup server with docker image

---
+++ server/main.py
@@ -0,0 +1,89 @@
+import os
+import re
+import sys
+import tempfile
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, Query
+from fastapi.params import Depends
+from whoosh import scoring
+from whoosh.fields import Schema, ID, TEXT, NGRAM
+from whoosh.index import create_in, FileIndex
+from whoosh.qparser import QueryParser
+import requests
+from whoosh.query import Regex, Prefix, Or
+
+SOURCE = os.environ.get("CRS_SOURCE", 'crs-list.txt')
+
+
+def build_search_index():
+    index_dir = tempfile.mkdtemp()
+    #schema = Schema(uri=ID(stored=True))
+    schema = Schema(uri=NGRAM(minsize=2, maxsize=6, stored=True))
+    idx = create_in(index_dir, schema)
+    writer = idx.writer()
+
+    print(f'Loading source from {SOURCE}...', end='', file=sys.stderr, flush=True)
+    if re.match(r'^https?://.*', SOURCE):
+        r = requests.get(SOURCE, stream=True)
+        r.raise_for_status()
+        for line in r.iter_lines(decode_unicode=True):
+            if not line.startswith('#'):
+                writer.add_document(uri=line.strip().replace('https://www.opengis.net/def/crs/', ''))
+    else:
+        with open(SOURCE) as f:
+            for line in f:
+                if not line.startswith('#'):
+                    writer.add_document(uri=line.strip().replace('https://www.opengis.net/def/crs/', ''))
+    writer.commit()
+    print(' [ok]', file=sys.stderr, flush=True)
+    return idx
+
+
+async def get_search_index() -> FileIndex:
+    return app.state.idx
+
+
+@asynccontextmanager
+async def app_lifespan(app: FastAPI):
+    app.state.idx = build_search_index()
+    yield
+
+
+app = FastAPI(lifespan=app_lifespan)
+
+
+@app.get('/search')
+def search(q: str = Query(..., min_length=1), idx: FileIndex = Depends(get_search_index)):
+    q = re.sub(r'[^A-Za-z0-9_/.,-]', '', q)
+    results = []
+    print('START!!', q, file=sys.stderr, flush=True)
+    with idx.searcher(weighting=scoring.Frequency) as searcher:
+        # Search for substring match
+        #regex_query = Regex("uri", f".*{re.escape(q)}.*")
+        #prefix_query = Prefix("uri", q)
+        #query = Or([prefix_query, regex_query])
+        query = QueryParser("uri", idx.schema).parse(q)
+        raw_results = searcher.search(query, limit=20)
+
+        # Prioritize prefix matches
+        for hit in raw_results:
+            uri = hit["uri"]
+            score = hit.score
+            if uri.startswith(q):
+                score += 10  # Boost prefix matches
+            if not re.match(r'^https?://.*', uri):
+                uri = f'https://www.opengis.net/def/crs/{uri}'
+            results.append({"uri": uri, "score": score})
+
+        # Sort by boosted score
+        results.sort(key=lambda x: -x["score"])
+    print('END!!', q, file=sys.stderr, flush=True)
+
+    return {"query": q, "results": results}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False, lifespan="on")
